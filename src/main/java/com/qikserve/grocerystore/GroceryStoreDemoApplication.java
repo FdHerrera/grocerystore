3c223238aeb9ab5e6628c8c9981ea460de26833a
Federico Herrera <federico.herrera_dev@outlook.com>
2024-04-20 15:06:48 -0300

    Init project

---
+++ src/main/java/com/qikserve/grocerystore/GroceryStoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qikserve.grocerystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryStoreDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qikserve/grocerystore/GroceryStoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qikserve.grocerystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
